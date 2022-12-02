d4b988bc157c0ab357e4bf19d53fd4eb0e6197e8
MabeDiaby <diabymabe@gmail.com>
2022-12-01 19:46:53 -0500

    days in month

---
+++ notes/notes.py
@@ -0,0 +1,8 @@
+# Functions with Outputs
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didnt provide valid inputs."
+    formated_name = f_name.title()
+    formated_last_name = l_name.title()
+    return f"{formated_name} {formated_last_name}"
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
